2be5280edb7d01cba8db49141932a4fe1c7434ae
Anthony Howe <anthowe33@gmail.com>
2020-05-13 20:26:48 -0400

    Second commit

--- app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
+++ app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
@@ -15,6 +15,34 @@
  */
 
 package com.example.android.trackmysleepquality
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase(){
+
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+    companion object{
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+        fun getInstance(context: Context): SleepDatabase {
+            synchronized(this){
+                var instance = INSTANCE
+                if (instance == null){
+                    instance = Room.databaseBuilder(context.applicationContext, SleepDatabase::class.java,
+                            "sleep_history_database").fallbackToDestructiveMigration().build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
 //
 //import androidx.room.Room
 //import androidx.test.ext.junit.runners.AndroidJUnit4
